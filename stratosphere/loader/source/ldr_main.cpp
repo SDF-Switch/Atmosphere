7bd152ef4967bd17e40547c9b76834a79b2e6942
tumGER <25822956+tumGER@users.noreply.github.com>
2018-11-09 16:13:16 +0100

    Revert "Implement Loader Changes"
    This reverts commit 9c32693a84547e76327160cd8c70e19db7666eea.

--- stratosphere/loader/source/ldr_main.cpp
+++ stratosphere/loader/source/ldr_main.cpp
@@ -95,9 +95,6 @@ void __appExit(void) {
 
 int main(int argc, char **argv)
 {
-    g_override_key_combination = KEY_R;
-    g_override_by_default = true;
-
     consoleDebugInit(debugDevice_SVC);
         
     /* TODO: What's a good timeout value to use here? */
@@ -107,8 +104,6 @@ int main(int argc, char **argv)
     server_manager->add_waitable(new ServiceServer<ProcessManagerService>("ldr:pm", 1));
     server_manager->add_waitable(new ServiceServer<ShellService>("ldr:shel", 3));
     server_manager->add_waitable(new ServiceServer<DebugMonitorService>("ldr:dmnt", 2));
-    server_manager->add_waitable(new ServiceServer<LoaderConfigService>("ldr:cfg", 4));
-
     if (!kernelAbove300()) {
         /* On 1.0.0-2.3.0, Loader services ldr:ro instead of ro. */
         server_manager->add_waitable(new ServiceServer<RelocatableObjectsService>("ldr:ro", 0x20));
